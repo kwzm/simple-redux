68cb87e1b80a05f3c5a516dff6e91aef5c46d19e
郑聪 <931745526@qq.com>
2019-05-16 09:53:08 +0800

    add index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import createStore from './createStore'
+import compose from './compose'
+import combineReducers from './combineReducers'
+import bindActionCreators from './bindActionCreators'
+import applyMiddleware from './applyMiddleware'
+
+export default {
+  createStore,
+  compose,
+  combineReducers,
+  bindActionCreators,
+  applyMiddleware
+}
